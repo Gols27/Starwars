b7fa822f88bb7527cf04d9a0ccee0d4fe4210e1f
Govil Gupta <govilgupta@in.ibm.com>
2019-07-13 15:21:29 +0530

    search page pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 import { AgGridModule } from 'ag-grid-angular';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { searchNamePipe } from '../searchName.pipe';
 
 @NgModule({
   declarations: [
@@ -21,17 +19,15 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     SearchComponent,
     LoginComponent,
     PageNotFoundComponent,
+    searchNamePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BsDropdownModule.forRoot(),
-    TabsModule.forRoot(),
     AgGridModule.withComponents([]),
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
-    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,18 +11,6 @@ export class SearchComponent implements OnInit {
  title: string =  'Search Planets';
  searchText: string;
  planetList: any[];
- heroes = [
-   { id: 11, name: 'Mr. Nice', country: 'India' },
-   { id: 12, name: 'Narco' , country: 'USA'},
-   { id: 13, name: 'Bombasto' , country: 'UK'},
-   { id: 14, name: 'Celeritas' , country: 'Canada' },
-   { id: 15, name: 'Magneta' , country: 'Russia'},
-   { id: 16, name: 'RubberMan' , country: 'China'},
-   { id: 17, name: 'Dynama' , country: 'Germany'},
-   { id: 18, name: 'Dr IQ' , country: 'Hong Kong'},
-   { id: 19, name: 'Magma' , country: 'South Africa'},
-   { id: 20, name: 'Tornado' , country: 'Sri Lanka'}
- ];
   constructor(private http: HttpClient) {
 
   }
@@ -30,7 +18,14 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
     this.http.get('https://swapi.co/api/planets/')
     .subscribe((response: any) => {
-      this.planetList = response.results;
+      this.planetList =  response.results.map(item => {
+         if (item.population === 'unknown'){
+           item.population = '0';
+         }
+         return item;
+       }).sort((a, b) => {
+          return b.population - a.population;
+        });
     }, error => {
       console.log(error);
     });
